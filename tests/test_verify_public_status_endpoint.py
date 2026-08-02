7893c326ff5bab5e8df8b38afb949ebc129d6902
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2026-08-01 19:10:43 -0700

    implemented the status endpoint test

--- tests/test_verify_public_status_endpoint.py
+++ tests/test_verify_public_status_endpoint.py
@@ -1,4 +1,26 @@
-
+import requests
 
 def test_verify_public_status_endpoint():
-    assert True
+    """
+    Example response body:
+        {
+    "app_name": "SuperSQA Job Tracker",
+    "api_version": "v1",
+    "environment": "local",
+    "server_time": "2026-08-02T01:55:28.264940Z"
+    }
+    """
+    # make the call
+    url = "http://localhost:3050/api/v1/public/status"
+    response = requests.get(url)
+
+    # vererify the status code
+    status_code = response.status_code
+    assert status_code == 200, f"API /api/v1/public/status is not returning a 200 status code. Actual status code: {status_code}"
+
+    # verify the body
+    response_body = response.json()
+    assert response_body["app_name"] == "SuperSQA Job Tracker"
+    assert response_body["api_version"] == "v1"
+    assert response_body["environment"] == "local", f"The environment expected in the api resposne but the value is {response_body['environment']}"
+    assert response_body["server_time"], "The date time expected in the api resposne but the value is empty or null"
